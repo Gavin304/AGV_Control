aadd3007ea4a8139f9551b58f1e46e59d0833a14
Gavin <gavin.arpandy@gmail.com>
2024-12-10 13:06:53 +0800

    Added Lidar packages

--- build/js2fork/prefix_override/sitecustomize.py
+++ build/js2fork/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/asrlab/sample_code_agv/install/js2fork'
+    sys.prefix = sys.exec_prefix = '/home/gavin/AGV_Control/install/js2fork'
---
+++ build/lakibeam1/CMakeFiles/lakibeam1_scan_node.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gavin/AGV_Control/src/Lakibeam_ROS2_Driver-main/src/lakibeam1_scan.cpp" "CMakeFiles/lakibeam1_scan_node.dir/src/lakibeam1_scan.cpp.o" "gcc" "CMakeFiles/lakibeam1_scan_node.dir/src/lakibeam1_scan.cpp.o.d"
+  "/home/gavin/AGV_Control/src/Lakibeam_ROS2_Driver-main/src/remote.cpp" "CMakeFiles/lakibeam1_scan_node.dir/src/remote.cpp.o" "gcc" "CMakeFiles/lakibeam1_scan_node.dir/src/remote.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
